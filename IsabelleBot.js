47b55e2453a685dc90aeb7f8fbed62df29051810
Aaron <aaronrules5alt@gmail.com>
2021-07-31 13:27:24 -0400

    Update IsabelleBot.js
    Remove unnecessary lines.

--- IsabelleBot.js
+++ IsabelleBot.js
@@ -4,11 +4,8 @@ was cleaned up by AaronRules5!
 Have fun!
 */
 
-
-const { exception } = require("console");
  const Discord = require("discord.js");
  const client = new Discord.Client();
- const fs = require("fs");
 
  client.on("ready", () => {client.user.setUsername("IsabelleBot");
   console.log(`Logged in as ${client.user.tag}!`);
