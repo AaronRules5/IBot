058c6bc1ce3a7caf4e01ae3192de05c7b0b13835
Aaron <aaronmstultz@yahoo.com>
2021-08-14 16:57:33 -0400

    Update FetchTest.js

--- FetchTest.js
+++ FetchTest.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const {Client, Intents} = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const colorCode=(x)=>{
     return "\x1b["+x.toString()+"m";
